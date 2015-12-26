8e270eb93f6179e411e7ab55fe97e8da4875e8d9
juliaranne <juliaarobinson@hotmail.com>
2015-12-26 19:39:02 +0000

    Randomise header image
    Random header image and fix issue with picking genre. Colour returns to
    blue when no genre is selected.

--- js/script.js
+++ js/script.js
@@ -80,14 +80,18 @@ function filmdetails(event){
 		 $('.filmtext').css('backgroundColor', '#00E3E3');
 
 		$('.film').each(function(){
+
+			var matchgenre = $(this).find('.filmoverlay');
+			var desc = $(this).parent('.film_wrapper');
 		
 			if ($(this).data('genre') === genre) {
-				
-				var matchgenre = $(this).find('.filmoverlay');
-				var desc = $(this).parent('.film_wrapper');
 				matchgenre.css('fill', '#FF7474');
 				desc.css('backgroundColor', '#FF7474');
-			} 
+			 } else if (genre === 'genre') {
+			 	matchgenre.css('fill', '#00E3E3');
+				desc.css('backgroundColor', '#00E3E3');
+			 }
+
 		});
 	});
 
@@ -179,8 +183,28 @@ $('select.dates').on('change', function(event){
  // });
 
  // };
+ var header = $('header');
 
- // imageswap();
+ var imgArray = [
+ "url('../lff/images/black-mass-01.jpg')",
+ "url('../lff/images/departure-03.jpg')",
+ "url('../lff/images/bigger-splash-02.jpg')",
+ "url('../lff/images/ayanda-05.jpg')",
+ "url('../lff/images/der-nachtmar-02.jpg')",
+ "url('../lff/images/an-03.jpg')",
+ "url('../lff/images/invention-03.jpg')",
+ "url('../lff/images/shooting-stars-01.jpg')",
+ "url('../lff/images/suffragette-002.jpg')",
+ "url('../lff/images/valley-of-love-04.jpg')"];
+
+function randomImg() {
+    var randomNum = (Math.floor((Math.random() * 1000000000)) % (imgArray.length));
+    // document.body.style.backgroundImage = imgArray[randomNum];
+    header.css('background-image', imgArray[randomNum]);
+
+}
+
+ window.onload = randomImg;
 
 
 
